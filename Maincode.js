c7c57f90b8b946e16f0dab6323d8b45e075ba23b
Guilherme606316 <guilhermemoraesx7@gmail.com>
2025-03-30 14:30:49 -0300

    Create Maincode.js
    Aqui será o código front-end principal do clone do Google.

---
+++ Maincode.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+
+function F1() {
+
+    const [cor, setcor] = useState(“blue”);
+
+const [x, setx] = useState(true);
+
+    const [pesquisa, setpesquisa] = useState(null);
+
+
+    F4() {
+    
+    setcor(“blue”);
+
+    window.location.href = “https://www.google.com/”
+
+    setx(!x);
+
+    };
+
+        
+    F3() {
+        document
+        const g = !cor;
+
+    F2(e) {
+
+        e.preventDefault();
+    };
+
+    return(
+    <header>
+        <button></button>
+
+        <button id=”1” onClick= {F4}” >todas</button>
+    
+        
+<button id=”1” onClick=”https://www.google.com/webhp” >imagens</button>
+        {x ? F4}
+
+        <button onClick=></button>
+        <button onClick=></button>
+    </header>
+
+    <main>
+
+        <h1>Google</h1>
+
+        <form onSubmit={F2}>
+
+        <input type=”text” placeholder=”pesquisar” id=”1” value={pesquisa} onChange={(e) =>Setpesquisa(e.target.value)}>
+
+        <button onClick=</button>
+        <button onClick=</button>
+        <button onClick=</button>
+        <button onClick=</button>
+
+
+        </form>
+    </main>
+
+    <footer>
+        <p> Brasil </p>
+
+        <button onClick=”> tema escuro: desativado/ativado</button>
+
+        <button onClick=> configurações</button>
+
+        <button onClick=”https://policies.google.com/privacy?hl=pt-BR&fg=1”> privacidade</button>
+
+        <button onClick=”https://policies.google.com/terms”> termos </button>
+
+        <button onClick=”https://ads.google.com/intl/pt-BR_br/home/”>publicidade</button>
+
+        <button onClick=”https://www.google.com/business/”>negócios </button>
+
+        <button onClick=”https://about.google/”> sobre</button>
+
+
+    );
+};
